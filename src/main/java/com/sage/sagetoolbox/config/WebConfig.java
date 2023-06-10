e1e6155bcff8c32a62f7cfd92382e932b4664d4e
bananeira <ijamil.osoria@gmail.com>
2023-06-11 00:52:59 +0200

    Adjust Cors mapping

--- src/main/java/com/sage/sagetoolbox/config/WebConfig.java
+++ src/main/java/com/sage/sagetoolbox/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("https://me.sorata.dev/", "http://localhost:4200/", "https://uhh.antimoxs.dev/")
+                .allowedOrigins("https://sora.coffee/", "http://localhost:4200/", "https://uhh.antimoxs.dev/")
                 .allowedMethods("*")
                 .maxAge(3600L)
                 .allowedHeaders("*")
